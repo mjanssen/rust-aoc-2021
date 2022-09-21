e4372f9095928908c97bb87fef8052a6aef2c67a
mjanssen <dev.marnix@gmail.com>
2022-09-21 08:57:43 +0200

    Fix typing for methods hashmap

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ pub mod lib;
 use regex::Regex;
 use std::{collections::HashMap, env};
 
-use crate::days::day_1::execute;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -20,9 +18,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         return Err("Supplied day was not correct, use 'day1' for example".into());
     }
 
-    let mut methods = HashMap::new();
+    println!("Results for {}", day);
 
-    methods.insert(String::from("day1"), execute);
+    let mut methods: HashMap<String, fn() -> Result<(), Box<dyn std::error::Error>>> =
+        HashMap::new();
+
+    methods.insert(String::from("day1"), days::day_1::execute);
+    methods.insert(String::from("day2"), days::day_2::execute);
 
     let method = match methods.get(day) {
         Some(function) => function,
