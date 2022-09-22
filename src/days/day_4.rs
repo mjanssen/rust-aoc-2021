e6a893ed75270e0d8f813d804bb15d466c11e88a
mjanssen <dev.marnix@gmail.com>
2022-09-22 08:24:27 +0200

    Added line picking for day 4

--- src/days/day_4.rs
+++ src/days/day_4.rs
@@ -1,3 +1,16 @@
+use crate::lib::load_file::load_data_file;
+
 pub fn execute() -> Result<(), Box<dyn std::error::Error>> {
+    let data = load_data_file("day4_example.txt")?;
+    let lines: Vec<&str> = data.split("\n").collect();
+
+    // First line are throws seperated by comma
+    let throws = &lines[0];
+
+    // Rest of lines are the playing boards
+    let boards = &lines[2..=lines.len() - 1];
+
+    println!("{:?}", throws);
+
     Ok(())
 }
